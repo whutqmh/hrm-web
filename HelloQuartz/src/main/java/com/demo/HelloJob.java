c994ea8cfe9029ea75acd4e26b75d19281797c2d
yonghu <yonghu@yonghu-PC>
2017-10-18 21:34:39 +0800

    Triger定时启动关闭

--- HelloQuartz/src/main/java/com/demo/HelloJob.java
+++ HelloQuartz/src/main/java/com/demo/HelloJob.java
@@ -3,26 +3,36 @@ package com.demo;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.naming.Context;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.Trigger;
 
 /**
  * @since
- * @param 
+ * @param
  * @return
  * @auther mhqi
  * @version 创建时间：2017年10月16日下午8:53:27
  */
-public class HelloJob implements Job{
+public class HelloJob implements Job {
 
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+	public void execute(JobExecutionContext context) throws JobExecutionException {
 		// 打印当前的执行时间，格式为2017-01-01 00:00:00
 		Date date = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println("当前执行时间为：" + sf.format(date));
 		// 编写具体的业务逻辑
 		System.out.println("Hello World!");
+		Trigger currentTrriger = context.getTrigger();
+		System.out.println("Start Time is:" + sf.format(currentTrriger.getStartTime()));
+		System.out.println("End Time is:" + sf.format(currentTrriger.getEndTime()));
+		JobKey jobKey = currentTrriger.getJobKey();
+		System.out.println("jobKey info---" + "JobName" + jobKey.getName() + 
+				"jobGroup" + jobKey.getGroup());
 	}
 
 }
--- HelloQuartz/src/main/java/com/demo/HelloScheduler.java
+++ HelloQuartz/src/main/java/com/demo/HelloScheduler.java
@@ -23,22 +23,29 @@ import org.quartz.impl.StdSchedulerFactory;
 public class HelloScheduler {
 
 	public static void main(String[] args) throws SchedulerException {
+		// 打印当前的时间，格式为2017-01-01 00:00:00
+		Date date = new Date();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("当前时间为：" + sf.format(date));
 		// 创建一个JobDetail实例，将该实例与HelloJob class绑定
 		JobDetail jobDetail = JobBuilder.newJob(HelloJob.class).withIdentity("myjob", "group1").build();
+		// 创建距离当前时间3秒后的时间
+		date.setTime(date.getTime() + 3000);
+		// 创建距离当前时间6秒后的时间
+		Date endDate = new Date();
+		endDate.setTime(endDate.getTime() + 6000);
 		// 创一个Trigger实例，定义该job立即执行并且每隔2秒执行一次
 		Trigger trigger = TriggerBuilder.newTrigger()
 				.withIdentity("myTrigger", "group1")
-				.startNow()
+				.startAt(date)
+				.endAt(endDate)
 				.withSchedule(SimpleScheduleBuilder.simpleSchedule()
 				.withIntervalInSeconds(2).repeatForever()).build();
 		// 創建Schedule实例
 		SchedulerFactory sFactory = new StdSchedulerFactory();
 		Scheduler scheduler = sFactory.getScheduler();
 		scheduler.start();
-		// 打印当前的时间，格式为2017-01-01 00:00:00
-		Date date = new Date();
-		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("当前时间为：" + sf.format(date));
+		
 		scheduler.scheduleJob(jobDetail, trigger);
 	}
 
